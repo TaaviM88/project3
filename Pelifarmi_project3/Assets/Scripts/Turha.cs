c95e74a12edd2f6a94fb86314d0ba44742c003f9
Mutti88 <juomakuski@hotmail.com>
2017-10-30 10:29:07 +0200

    Alustus

---
+++ Pelifarmi_project3/Assets/Scripts/Turha.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turha : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Turha ainoastaan githubin ignorea varten
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
